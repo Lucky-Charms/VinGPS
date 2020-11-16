a34a1a1df0e3a57d1e055c16d59a495f2fbb3620
Marchael Acode <m.acode@outlook.com>
2020-11-16 10:55:10 -0800

    Creating activity files

---
+++ app/src/main/java/com/luckycharms/vingps/activities/VehicleListActivity.java
@@ -0,0 +1,16 @@
+package com.luckycharms.vingps.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.luckycharms.vingps.R;
+
+public class VehicleListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vehicle_list);
+    }
+}
